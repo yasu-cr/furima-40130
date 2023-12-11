7f5f4f4326029008001cf7af82a211220dfeb77f
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-11 18:33:58 +0900

    新規登録でフォーム入力の必須条件を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name, presence: true
-         validates :last_name_kana, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ } 
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_10_163059) do
     t.string "last_name", null: false
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
