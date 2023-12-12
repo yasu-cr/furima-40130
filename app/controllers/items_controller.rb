ad3a2370575473fb679f9354e168fbbacf3530af
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-12 17:37:10 +0900

    Active Storageを導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+  params.require(:item).permit(:product, :image).merge(user_id: current_user.id)
+  end
+
 end
