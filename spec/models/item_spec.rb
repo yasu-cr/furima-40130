a5ebeffa9dfb7c747f1ff61e84dae394eb1453fe
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-17 19:16:31 +0900

    priceに関するテストコードを行った

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -52,5 +52,20 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Image can't be blank")
     end
+    it 'priceが半角数字以外が含まれるとき' do
+      @item.price = '１０００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it 'priceが300未満のとき' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+    it 'priceが10000000以上のとき' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+    end
   end
 end
