ec587e897a2c709f3145323d227259a652c5d87d
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-19 15:53:07 +0900

    編集して保存できるようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_tweet, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_tweet, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,6 +20,23 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user == current_user
+      render "edit"
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
