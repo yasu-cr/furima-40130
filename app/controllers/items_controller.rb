07b6ed5606d9fcb573d361f7c3aeb60157c338b9
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-19 18:17:58 +0900

    Rubocopを実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,9 +39,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user_id
-    @item.destroy
-    redirect_to root_path
+    if current_user == @item.user
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
     end
   end
 
