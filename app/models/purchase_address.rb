91a6515bd52df8d25af2c26fada4b08d4985f6fe
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-20 14:35:14 +0900

    データをテーブルに保存する処理

---
+++ app/models/purchase_address.rb
@@ -0,0 +1,23 @@
+class PurchaseAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :shipping_region_id, :city, :street_address, :building_name, :telephone_number, :user_id, :item_id
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :street_address
+    validates :telephone_number
+    validates :user_id
+    validates :item_id
+    
+  end
+  validates :shipping_region_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+ 
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+
+    Address.create(postal_code: postal_code, shipping_region_id: shipping_region_id, city: city, street_address: street_address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase_id)
+  end
+
+end
