2f9dd5a7c7f84716b2ea3cdb0ce33b25380dc6a9
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-20 15:57:02 +0900

    Formオブジェクトのインスタンスを生成する記述した

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,6 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
-
-  def new
-    @purchase_address = PurchaseAddress.new
-  end
+  before_action :set_item, only: [:index, :create]
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
@@ -15,7 +12,15 @@ class PurchasesController < ApplicationController
     end
   end
 
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def purchase_params
     params.require(:purchase_address).permit(:postal_code, :shipping_region_id, :city, :street_address, :building_name, :telephone_number).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: 'items#index'
 
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :purchase, only: [:index, :create]
+  end
 end
