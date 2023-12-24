a2235956c59cde5ae235ee56c6996f48dfb0e7c4
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2023-12-24 17:12:55 +0900

    JavaScriptを読み込みできるように修正した

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -33,8 +33,8 @@ class PurchasesController < ApplicationController
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      amount: @item.price,  # 商品の値段
+      card: purchase_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
