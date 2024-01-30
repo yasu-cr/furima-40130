f5e0df53cca278ebc50e65294f48f565a860d46a
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2024-01-30 22:20:13 +0900

    div要素とimg要素を生成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -7,16 +7,21 @@ document.addEventListener('turbo:load', function(){
 
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!itemForm) return null;
-  console.log("preview.jsが読み込まれました");
 
   // input要素を取得
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', function(e){
-    console.log("input要素で値の変化が起きました");
-    console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+
+
+   // 画像を表示するためのdiv要素を生成
+   const previewWrapper = document.createElement('div');
+   previewWrapper.setAttribute('class', 'preview');
+
+   // 表示する画像を生成
+   const previewImage = document.createElement('img');
+   previewImage.setAttribute('class', 'preview-image');
   });  
 });
