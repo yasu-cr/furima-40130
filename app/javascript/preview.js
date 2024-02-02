aedfae85d57f1141d5a20a8736e2bc18090b6308
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2024-02-02 19:33:09 +0900

    削除ボタンから画像を削除できるようにした

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -18,8 +18,17 @@ document.addEventListener('turbo:load', function(){
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src', blob);
 
+    // 削除ボタンを生成
+    const deleteButton = document.createElement("div");
+    deleteButton.setAttribute("class", "image-delete-button");
+    deleteButton.innerText = "削除";
+
+    // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
+    deleteButton.addEventListener("click", () => deleteImage(dataIndex));    
+
    // 生成したHTMLの要素をブラウザに表示させる
    previewWrapper.appendChild(previewImage);
+   previewWrapper.appendChild(deleteButton);
    previewList.appendChild(previewWrapper);
   };
 
